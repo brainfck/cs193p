101293a29df047bf26006bc46b6395d953d3e470
brainfck <mykhailo.tsuber@gmail.com>
2024-07-29 15:51:34 +0200

    initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,12 @@
+// Created by brainfck on 7/29/24.
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
